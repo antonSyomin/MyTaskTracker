015fab1d55939542908829115458521191113305
Anton <anton.syomin@skillbox.ru>
2024-07-17 02:36:33 +0300

    included general template form to create.blade.php and edit.blade.php

--- resources/views/card/edit.blade.php
+++ resources/views/card/edit.blade.php
@@ -4,16 +4,7 @@
 
 @section('content')
 {{ html()->modelForm($card, 'PATCH', route('cards.update', $card))->open() }}
-{{  html()->label('Название', 'title') }}
-    {{  html()->input('text', 'title') }}
-    {{  html()->label('Контент', 'content') }}
-    {{  html()->input('text', 'content') }}
-    {{  html()->label('Владелец', 'owner_id') }}
-    {{  html()->input('text', 'owner_id') }}
-    {{  html()->label('Статус', 'status') }}
-    {{  html()->textarea('status') }}
-    {{  html()->label('Срок', 'deadline') }}
-    {{  html()->textarea('deadline') }}
+    @include('card.form')
     {{ html()->submit('Обновить') }}
 {{ html()->closeModelForm() }}
 @endsection
