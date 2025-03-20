d221c832ac376eb16a1ad01acf3a50bcffb662d0
anton <seminanton2@gmail.com>
2025-03-20 18:55:34 +0300

    добавил плохого кода

--- resources/views/pages/User/Login.vue
+++ resources/views/pages/User/Login.vue
@@ -1,8 +1,23 @@
 <script setup>
 console.log('qqq');
+
+function foo(n) {
+    return n * 100 - 15;
+}
 </script>
 <template>
     <div>
 
     </div>
 </template>
+<style scoped lang="scss">
+.scroller {
+  overflow-x: scroll;
+}
+.s-blockname {
+  display: block;
+}
+.s-blockname-elm {
+  background-color: var(--black);
+}
+</style>
