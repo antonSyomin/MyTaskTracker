a739109cdd15480ca70e5b04bc5cfbd2bb49f851
anton <seminanton2@gmail.com>
2025-03-20 18:27:27 +0300

    Пользователь авторизуется

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,13 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
-  resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-    return pages[`./Pages/${name}.vue`]
-  },
+  resolve: async name => {
+    const page = await resolvePageComponent(`../views/pages/${name}.vue`, import.meta.glob("../views/pages/**/*.vue"));
+    //page.default.layout ??= Admin;
+    return page;
+},
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
--- resources/views/pages/User/Login.vue
+++ resources/views/pages/User/Login.vue
@@ -2,8 +2,13 @@
 </script>
 <template>
     <div>
-
+        <form>
+            <el-input type="username" v-model="form.username" />
+            <el-input type="password" v-model="form.username" />
+            <button class="g-button">Войти</button>
+        </form>
     </div>
 </template>
 <script>
+import { ElInput, ElSwitch } from "element-plus";
 </script>
