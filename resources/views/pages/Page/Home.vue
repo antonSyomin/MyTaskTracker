7a2177f1badd62c18263504183616259275531df
anton <seminanton2@gmail.com>
2025-03-21 15:22:15 +0300

    Ужасный код

--- resources/views/pages/Page/Home.vue
+++ resources/views/pages/Page/Home.vue
@@ -1,8 +1,17 @@
 <script setup>
+console.log('qq');
+function testOperators()
+{
+    let p;
+    //const moment = dayjs.utc(gmtTime).tz(dayjs.tz.guess());
+    //return moment.format('DD.MM.YYYY ') + (separator ? separator + ' ': '') + moment.format('HH:mm');
+    const a = 5 + 6;
+    const b = 'hello';
+    return true;
+}
 </script>
-
 <template>
     <p>Привеееет!</p>
 </template>
-<style>
+<style scoped>
 </style>
