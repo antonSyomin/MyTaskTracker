0de11c93396cdf34a85c8cc4a3a368bcf0e5c563
anton <seminanton2@gmail.com>
2025-03-21 11:12:45 +0300

    Плохой код

--- resources/views/pages/User/Login.vue
+++ resources/views/pages/User/Login.vue
@@ -1,9 +1,29 @@
+<style scoped lang="scss">
+.s-login {
+  display: block;
+
+  &-content {
+    overflow-x: scroll;
+    display: block;
+  }
+
+  &-element {
+    background-color: var(--black);
+  }
+}
+</style>
 <script setup>
+console.log('qqq');
+
+// Безымянная функция для вывода числа
+function foo(n) {
+    return n * 100 - 15;
+}
 </script>
 <template>
-    <div>
-
+    <div class="s-login">
+        <div class="s-login__element">
+            {{ calculatePriceWithDiscount(1) }}
+        </div>
     </div>
 </template>
-<script>
-</script>
